0713b0804553319808c2c4b1ee648768a2e8ff46
lixz <lixz@wanbu.com.cn>
2017-12-08 09:15:37 +0800

    添加模块启动Application

--- src/main/java/com/yushan/module_login/activity/LoginActivity.java
+++ src/main/java/com/yushan/module_login/activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.yushan.module_login;
+package com.yushan.module_login.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
---
+++ src/main/java/com/yushan/module_login/debug/LoginApplication.java
@@ -0,0 +1,18 @@
+package com.yushan.module_login.debug;
+
+import android.os.Bundle;
+
+import com.yushan.lib_base.base.BaseActivity;
+import com.yushan.lib_base.base.BaseApplication;
+
+/**
+ * Created by beiyong on 2017-12-8.
+ */
+
+public class LoginApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
